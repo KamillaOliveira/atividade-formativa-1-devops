d60277ae59731a7df12772a1436b57a4d382a9f2
Kamilla Oliveira <kamillacoliv1998@gmail.com>
2026-08-26 19:34:05 -0300

    feat: adiciona escudo divino do Paladino

--- paladino.py
+++ paladino.py
@@ -1,3 +1,4 @@
     def __init__(self, nome): self.nome = nome; self.hp = 100
         self.faccao = 'Alliance'; self.mana = 100
     def usar_holy_light(self): self.hp += 25; self.mana -= 15
+    def usar_divine_shield(self): return 'Imune a dano por 8 segundos'
